49d314acae0778fa97ab78af0c9a7458de3849b6
Xamnalim <mlapinski98@gmail.com>
2025-06-05 22:20:47 +0200

    feat: print books/frankenstein.txt contents

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def main():
+    file_contents = get_book_text('books/frankenstein.txt')
+    print(file_contents)
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+main()
