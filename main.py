581075653994017930754372550f0da21382eaac
Xamnalim <mlapinski98@gmail.com>
2025-06-06 17:35:01 +0200

    feat: handle cli arguments

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from stats import get_word_count, get_letter_counts, sort_letter_counts
+import sys
 
 
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_contents = get_book_text(sys.argv[1])
     num_words = get_word_count(file_contents)
     letter_counts = get_letter_counts(file_contents)
     letter_counts_sorted = sort_letter_counts(letter_counts)
